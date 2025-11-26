82fa297ceffde9cda93251c5a40d297c2fa52e3f
RaviCoderWallah <raviverma05042023@gmail.com>
2025-11-26 21:01:23 +0530

    first question try

--- Day7-Prime-Factorization/script.js
+++ Day7-Prime-Factorization/script.js
@@ -1,2 +1,17 @@
+function calculatePrimeFactorization(num) {
+
+}
+
+console.log(calculatePrimeFactorization(48));
+
+/* 
+2 | 48 | 24
+
+2  - Divisor
+48 - Dividend 
+24 - Quotient 
+0 - Remaninder 
+
+*/
 
 
